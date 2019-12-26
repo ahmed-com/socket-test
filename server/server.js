c468fff357e587e396859b26c769aefca00031fe
Ahmed Grwan <ahmed0grwan@gamil.com>
2019-12-27 01:41:29 +0200

    initial message

---
+++ server/server.js
@@ -0,0 +1,10 @@
+//we do this step so that we don't have to go back every time
+const path = require('path');
+const express = require('express');
+const publicPath = path.join(__dirname,'/../public');
+var app = express();
+app.use(express.static(publicPath));
+const port =process.env.PORT || 3000;
+app.listen(port,()=>{
+    console.log(`server is up at port ${port}`)
+});
