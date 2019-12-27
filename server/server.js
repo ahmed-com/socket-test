4dc351ebf57b484748afba298585fd7c754fce92
Ahmed Grwan <ahmed0grwan@gamil.com>
2019-12-27 05:12:30 +0200

    server&client listening for connection and disconnection

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 //we do this step so that we don't have to go back every time
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
+
 const publicPath = path.join(__dirname,'/../public');
-var app = express();
-app.use(express.static(publicPath));
 const port =process.env.PORT || 3000;
-app.listen(port,()=>{
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
+
+app.use(express.static(publicPath));
+io.on('connection',socket =>{
+    console.log('a new user just connected');
+    socket.on('disconnect',()=> console.log('user just disconnected'));
+});//this method is basically an event listener and we here are listening for a connection event
+
+
+server.listen(port,()=>{
     console.log(`server is up at port ${port}`)
 });
